a0571e13f6eb563336065fe71c670ceb87f8af18
Mrigank Tiwari <tiwarimrigank2@gmail.com>
2025-07-29 20:15:33 +0530

    multi layer RNN for char level LM

--- rnn_lstm/rnn_lstm.py
+++ rnn_lstm/rnn_lstm.py
@@ -19,7 +19,10 @@ val_words = words[n:]
 batch_size = 64
 n_embd = 128
 hidden_dim = 128
-max_iters = 1000
+max_iters = 10000
+n_layers = 3
+eval_iters = 250
+eval_interval = 1000
 
 # get batch of data
 def get_batch(split, bs = batch_size):
@@ -39,42 +42,68 @@ def get_batch(split, bs = batch_size):
     out_seq_padded = pad_sequence(out_seq, batch_first=True, padding_value=-1)
     return inp_seq_padded, out_seq_padded
 
+# evaluate loss
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    # model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            x, y = get_batch(split)
+            logits = model(x)
+            loss = F.cross_entropy(logits.view(-1, vocab_size), y.view(-1), ignore_index=-1)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    # model.train()
+    return out
+
 # model class
-class RNN:
-    def __init__(self, n_embd=n_embd, hidden_dim=hidden_dim):
+class MultiLayerRNN:
+    def __init__(self, n_embd=n_embd, hidden_dim=hidden_dim, n_layers=n_layers):
         self.n_embd = n_embd
         self.hidden_dim = hidden_dim
+        self.n_layers = n_layers
 
         self.emb_layer = torch.randn((vocab_size, n_embd)) * 0.1 # (27, n_embd)
-        self.wxh = torch.randn(n_embd, hidden_dim) * 0.01 # (b,t,n_embd) -> (b,t,hidden_dim)
-        self.whh = torch.randn(hidden_dim, hidden_dim) * 0.01 # (b,t,hidden_dim) -> (b,t,hidden_dim)
-        self.bh  = torch.zeros(hidden_dim)
+
+        self.wxh = [torch.randn(n_embd if i==0 else hidden_dim, hidden_dim) * 0.01 for i in range(n_layers)] # (b,t,n_embd) -> (b,t,hidden_dim)
+        self.whh = [torch.randn(hidden_dim, hidden_dim) * 0.01 for _ in range(n_layers)] # (b,t,hidden_dim) -> (b,t,hidden_dim)
+        self.bh  = [torch.zeros(hidden_dim) for _ in range(n_layers)]
 
         self.why = torch.randn(hidden_dim, vocab_size) * 0.01 # (b,t,hidden_dim) -> (b,t,vocab_size)
         self.by  = torch.zeros(vocab_size)
 
-        self.parameters = [self.emb_layer, self.wxh, self.whh, self.bh, self.why, self.by]
+        self.parameters = [self.emb_layer , self.why, self.by]
+        for layer in range(n_layers):
+            self.parameters += [self.wxh[layer], self.whh[layer], self.bh[layer]]
+
+        for p in self.parameters:
+            p.requires_grad = True
 
     def __call__(self, x):
         # x: (b,t)
         B,T = x.shape
-        h = torch.zeros(B, self.hidden_dim)
+        hs = [torch.zeros(B, self.hidden_dim) for _ in range(self.n_layers)] # list of h for each layer in n_layers
         x_emb = self.emb_layer[x]
         logits = []
+        
         for t in range(T):
             xt = x_emb[:, t, :] # (b, n_embd)
-            h = torch.tanh(xt @ self.wxh + h @ self.whh + self.bh)
-            # (b, n_embd) @ (n_embd, hidden_dim) --> (b, hidden_dim) + (b, hidden_dim) @ (hidden_dim, hidden_dim) --> (b, hidden_dim)
-            yt = h @ self.why + self.by # (b, hidden_dim) @ (hidden_dim, vocab_size) --> (b, vocab_size)
+
+            for layer in range(n_layers):
+                h_prev = hs[layer]
+                hs[layer] = torch.tanh(xt @ self.wxh[layer] + h_prev @ self.whh[layer] + self.bh[layer]) # (b, hidden_dim)
+                # (b, n_embd) @ (n_embd, hidden_dim) --> (b, hidden_dim) + (b, hidden_dim) @ (hidden_dim, hidden_dim) --> (b, hidden_dim)
+                xt = hs[layer] # (b, hidden_dim)
+            yt = hs[-1] @ self.why + self.by # (b, vocab_size)
             logits.append(yt)
 
         logits = torch.stack(logits, dim=1) # (b, T, vocab_size)
         return logits
 
 # model init
-model = RNN()
-for p in model.parameters:
-    p.requires_grad = True
+model = MultiLayerRNN()
 print(f'{sum(p.numel() for p in model.parameters)} params')
 
 for iter in range(max_iters):
@@ -86,11 +115,12 @@ for iter in range(max_iters):
         p.grad = None
 
     loss.backward()
-    if iter % 100 == 0:
+    if iter % eval_interval == 0:
+        losses = estimate_loss()
         total_norm = torch.sqrt(sum((p.grad**2).sum() for p in model.parameters if p.grad is not None))
-        print(f'Iteration {iter} train loss = {loss.item():.4f} | Gradient norm: {total_norm:.4f}')
+        print(f'Iteration {iter} train loss = {losses['train']:.4f} | val loss = {losses['val']:.4f} | Gradient norm: {total_norm:.4f}')
 
-    lr = 0.1
+    lr = 0.1 if iter < 4000 else 0.01
     for p in model.parameters:
         p.data += -(lr * p.grad)
 
