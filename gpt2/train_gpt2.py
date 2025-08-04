85284255c59439584c0a6dcf75513b423de0d9ee
Mrigank Tiwari <tiwarimrigank2@gmail.com>
2025-08-04 11:59:15 +0530

    addition: data loader lite

--- gpt2/train_gpt2.py
+++ gpt2/train_gpt2.py
@@ -149,6 +149,40 @@ class GPT(nn.Module):
         return model
 
 # --------------------------------------------------------------------
+import tiktoken
+class DataLoaderLite:
+    def __init__(self, B, T):
+        self.b = B
+        self.t = T
+
+        # at init load the tokens from disk and load them in memory
+        enc = tiktoken.get_encoding('gpt2')
+        with open('../gpt/input.txt', 'r') as f:
+            text = f.read()
+        tokens = enc.encode(text)
+        self.tokens = torch.tensor(tokens)
+        print(f'loaded {len(self.tokens)} tokens')
+        print(f'1 epoch = {len(self.tokens) // (B*T)} batches')
+        self.current_position = 0
+
+        
+
+    def next_batch(self):
+        B,T = self.b, self.t
+        buf = self.tokens[self.current_position : self.current_position + B*T + 1]
+        x = buf[:-1].view(B,T)
+        y = buf[1:].view(B,T)
+
+        # advance the position in the tensor
+        self.current_position += B*T
+
+        # if loading the next batch would be out of bounds, reset
+        if self.current_position + (B*T + 1) > len(self.tokens):
+            self.current_position = 0
+        
+        return x,y
+
+# --------------------------------------------------------------------
 
 max_return_sequences = 5
 max_length = 30
@@ -159,24 +193,16 @@ elif hasattr(torch.backends, 'mps') and torch.backends.mps.is_available():
     device = 'mps'
 print(f'using device: {device}')
 
-# data loader
-enc = tiktoken.get_encoding('gpt2')
-with open('../gpt/input.txt', 'r') as f:
-    text = f.read()
-text = text[:1000]
-tokens = enc.encode(text)
-B,T = 4,32
-buf = torch.tensor(tokens[: B*T + 1])
-x = buf[:-1].view(B,T).to(device)
-y = buf[1:].view(B,T).to(device)
-
 # model = GPT.from_pretrained('gpt2')
 model = GPT(GPTConfig())
 model = model.to(device)
+train_loader = DataLoaderLite(B=4, T=32)
 
 # forward, backward, optimize
 optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3) # 3e-4
 for i in range(50):
+    x,y = train_loader.next_batch()
+    x,y = x.to(device), y.to(device)
     optimizer.zero_grad()
     logits, loss = model(x, y)
     loss.backward()
