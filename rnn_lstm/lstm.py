11c2098728c1cd1b2cf75e3f19ab8020e498c392
Mrigank Tiwari <tiwarimrigank2@gmail.com>
2025-08-05 20:24:34 +0530

    simplified lstm.py, train / generate in notebook

--- rnn_lstm/lstm.py
+++ rnn_lstm/lstm.py
@@ -3,61 +3,56 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.utils.rnn import pad_sequence
 
-#-------------------------------------------
-# data, vocab, mappings, splits
-words = open('../makemore/names.txt', 'r').read().splitlines()
-vocab = sorted(list(set(''.join(words))))
-stoi = {ch:i+1 for i,ch in enumerate(vocab)}
-stoi['.'] = 0
-itos = {i:ch for ch,i in stoi.items()}
-vocab_size = len(itos)
+class TextLoader:
+    def __init__(self, vocab, stoi, itos, train_data, val_data):
+        self.vocab = vocab
+        self.vocab_size = len(vocab)
+        self.stoi = stoi
+        self.itos = itos
+        self.train_data = train_data
+        self.val_data = val_data
+    
+    @classmethod
+    def from_text(cls, text, train_split=0.9):
+        vocab = sorted(list(set(text)))
+        stoi = {ch:i for i,ch in enumerate(vocab)}
+        itos = {i:ch for ch,i in stoi.items()}
 
-n = int(len(words) * 0.9)
-train_words = words[:n]
-val_words = words[n:]
+        data = [stoi[ch] for ch in text]
+        n = int(train_split * len(data))
+        train_data = data[:n]
+        val_data = data[n:]
+        return cls(vocab, stoi, itos, train_data, val_data)
 
-# hparams
-batch_size = 256
-n_embd = 64
-hidden_dim = 128
-max_iters = 20000
-n_layers = 2
-eval_iters = 250
-eval_interval = 1000
+    def encode(self, text):
+        return [self.stoi[ch] for ch in text]
 
-# get batch of data
-def get_batch(split, bs = batch_size):
-    inp_seq, out_seq = [], [] # list of sequence of chars in input and output (shifted by 1)
-    data = train_words if split == 'train' else val_words
-    ix = torch.randint(len(data)-1, (batch_size,))
-    batch = [data[i] for i in ix]
-    for word in batch:
-        chs = word + '.'
-        inp, out = [], []
-        inp = [stoi[ch1] for ch1,_ in zip(chs, chs[1:])]
-        out = [stoi[ch2] for _,ch2 in zip(chs, chs[1:])]
+    def decode(self, tokens):
+        return ''.join([self.itos[token] for token in tokens])
 
-        inp_seq.append(torch.tensor(inp))
-        out_seq.append(torch.tensor(out))
-    inp_seq_padded = pad_sequence(inp_seq, batch_first=True, padding_value=0)
-    out_seq_padded = pad_sequence(out_seq, batch_first=True, padding_value=-1)
-    return inp_seq_padded, out_seq_padded
+    def get_batch(self, split, batch_size=8, block_size=32):
+        self.block_size = block_size
+        split_data = self.train_data if split == 'train' else self.val_data
+        ix = torch.randint(0, len(split_data) - block_size, (batch_size,))
+        x = [split_data[i : i + block_size] for i in ix]
+        y = [split_data[i+1 : i + block_size + 1] for i in ix]
+        return torch.tensor(x), torch.tensor(y)
 
 # evaluate loss
 @torch.no_grad()
-def estimate_loss():
+def estimate_loss(model, data_loader, device, eval_iters=250):
     out = {}
-    # model.eval()
+    model.eval()
     for split in ['train', 'val']:
         losses = torch.zeros(eval_iters)
         for k in range(eval_iters):
-            x, y = get_batch(split)
+            x, y = data_loader.get_batch(split, batch_size=128, block_size=128)
             x, y = x.to(device), y.to(device)
             logits = model(x)
-            loss = F.cross_entropy(logits.view(-1, vocab_size), y.view(-1), ignore_index=-1)
+            loss = F.cross_entropy(logits.view(-1, data_loader.vocab_size), y.view(-1), ignore_index=-1)
             losses[k] = loss.item()
         out[split] = losses.mean()
-    # model.train()
+    model.train()
     return out
 
 class LSTMCell(nn.Module):
@@ -153,181 +148,3 @@ class MultiLayerLSTM(nn.Module):
             logits.append(yt) # T elements of shape (b,vocab_size)
         logits = torch.stack(logits, dim=1) # (B,T,vocab_size)
         return logits
-
-# -----------------------------------------------------------------------------------------------------
-
-device = 'mps'
-model = MultiLayerLSTM(vocab_size, n_layers=n_layers, input_size=n_embd, hidden_size=hidden_dim)
-model = model.to(device)
-params = model.parameters()
-print(f'{sum(p.numel() for p in params)} parameters')
-for p in params:
-    p.requires_grad = True
-
-                
-# # forward pass
-# xb, yb = get_batch('train')
-# logits = model(xb)
-# print(f'logits shape: {logits.shape}')
-
-# loss = F.cross_entropy(logits.view(-1, logits.shape[-1]), yb.view(-1), ignore_index=-1)
-# print(f'loss = {loss.item()}')
-
-# model training
-for iter in range(max_iters):
-    xb, yb = get_batch('train')
-    xb, yb = xb.to(device), yb.to(device)
-    logits = model(xb)
-    loss = F.cross_entropy(logits.view(-1, vocab_size), yb.view(-1), ignore_index=-1)
-
-    # zero the gradients
-    for p in model.parameters():
-        p.grad = None
-
-    loss.backward()
-
-    # print loss
-    if iter % eval_interval == 0:
-        losses = estimate_loss()
-        total_norm = torch.sqrt(sum((p.grad**2).sum() for p in model.parameters() if p.grad is not None))
-        print(f'Iteration {iter} train loss = {losses['train']:.4f} | val loss = {losses['val']:.4f} | Gradient norm: {total_norm:.4f}')
-
-    # weights update
-    lr = 1e-1 if iter < 20000 else 1e-2
-    for p in model.parameters():
-        p.data += -(lr * p.grad)
-
-# generate
-with torch.no_grad():
-    print(f'\nStarting generation')
-    print('--'*10)
-    for _ in range(10):
-        idx = torch.zeros(1,1, dtype=torch.long, device=device)
-        h = torch.zeros(1, hidden_dim, device=device)
-        # emb = emb_layer[idx]
-        # print(f'emb shape: {emb.shape}')
-        count = 0
-        while True:
-            logits = model(idx)[:,-1]
-            probs = F.softmax(logits, dim=-1)
-            next_idx = torch.multinomial(probs, num_samples=1).item()
-            idx = torch.cat([idx, torch.tensor([[next_idx]], device=device)], dim=1)
-            if next_idx == 0:
-                break
-        print(''.join([itos[i.item()] for i in idx[0][1:]]))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# -------------------------------------------------------------------------------------------
-# class SimpleLSTM:
-#     def __init__(self, n_embd=n_embd, hidden_dim=hidden_dim):
-#         self.n_embd = n_embd
-#         self.hidden_dim = hidden_dim
-
-#         self.emb_layer = torch.randn((vocab_size, n_embd)) * 0.1
-
-#         # forget gate
-#         self.wxf = torch.randn(n_embd, hidden_dim) * 0.01
-#         self.whf = torch.randn(hidden_dim, hidden_dim) * 0.01
-#         self.bf  = torch.zeros(hidden_dim)
-
-#         # input gate
-#         self.wxi = torch.randn(n_embd, hidden_dim) * 0.01
-#         self.whi = torch.randn(hidden_dim, hidden_dim) * 0.01
-#         self.bi  = torch.zeros(hidden_dim)
-
-#         # cell candidate gate
-#         self.wxg = torch.randn(n_embd, hidden_dim) * 0.01
-#         self.whg = torch.randn(hidden_dim, hidden_dim) * 0.01
-#         self.bg  = torch.zeros(hidden_dim)
-
-#         # output gate
-#         self.wxo = torch.randn(n_embd, hidden_dim) * 0.01
-#         self.who = torch.randn(hidden_dim, hidden_dim) * 0.01
-#         self.bo  = torch.zeros(hidden_dim)
-
-#         self.why = torch.randn(hidden_dim, vocab_size) * 0.01
-#         self.by  = torch.zeros(vocab_size)
-
-#         self.parameters = [
-#             self.emb_layer,
-#             self.wxf, self.whf, self.bf,
-#             self.wxi, self.whi, self.bi,
-#             self.wxg, self.whg, self.bg,
-#             self.wxo, self.who, self.bo,
-#             self.why, self.by
-#         ]
-
-#         for p in self.parameters:
-#             p.requires_grad = True
-
-#     def __call__(self, x):
-#         # x: (b,t)
-#         B,T = x.shape
-#         h = torch.zeros(B, self.hidden_dim) # hidden state (b,h)
-#         c = torch.zeros(B, hidden_dim) # cell state (b,h)
-
-#         x_emb = self.emb_layer[x] # (b,t,n_embd)
-#         logits = []
-#         for t in range(T):
-#             xt = x_emb[:, t, :] # (b,n_embd)
-#             ft = torch.sigmoid(xt @ self.wxf + h @ self.whf + self.bf) # (b,h)
-#             it = torch.sigmoid(xt @ self.wxi + h @ self.whi + self.bi) # (b,h)
-#             gt = torch.tanh(   xt @ self.wxg + h @ self.whg + self.bg) # (b,h)
-#             ot = torch.sigmoid(xt @ self.wxo + h @ self.who + self.bo) # (b,h)
-
-#             c = (ft * c) + (it * gt)
-#             h = ot * torch.tanh(c)
-
-#             yt = h @ self.why + self.by # (b,vocab_size)
-#             logits.append(yt) # list of T elements of shape (b,vocab_size)
-#         return torch.stack(logits, dim=1) # (b,T,vocab_size)
-# -------------------------------------------------------------------------------------------
