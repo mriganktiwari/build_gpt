5e671d54e05763f8db883fbfe920b257cd41b8f0
Mrigank Tiwari <tiwarimrigank2@gmail.com>
2025-08-13 00:23:15 +0530

    GPT2 skeleton ready, next up Block

--- gpt2/train_gpt2.py
+++ gpt2/train_gpt2.py
@@ -6,11 +6,24 @@ from dataclasses import dataclass
 @dataclass
 class GPTConfig:
     vocab_size: int = 50257
-    n_embd: int = 384
+    n_embd: int = 768
     n_layer: int = 12
     n_head: int = 12
     block_size: int = 1024
 
-c1 = GPTConfig(n_embd=128)
-c2 = GPTConfig()
-print(c1 == c2)
+config = GPTConfig()
+
+# model ----------------------------------------------------------------
+
+class GPT2(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+
+        self.transformer = nn.ModuleDict(dict(
+            wte = nn.Embedding(config.vocab_size, config.n_embd),
+            wpe = nn.Embedding(config.block_size, config.n_embd),
+            h   = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+            ln_f = nn.LayerNorm(config.n_embd)
+        ))
+        self.ln_head = nn.Linear(config.n_embd, config.vocab_size)
